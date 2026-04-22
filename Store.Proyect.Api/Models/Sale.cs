2a818fd19fdc5aae790c761ab8f6f07dacbcef96
maribel798-txt <lrs22110150@purisima.tecnm.mx>
2026-04-21 21:58:43 -0600

    En sale definir los tipos de atributos

---
+++ Store.Proyect.Api/Models/Sale.cs
@@ -0,0 +1,10 @@
+﻿namespace Store.Proyect.Api.Models;
+
+public class Sale
+{
+    public int Id { get; set; }
+    public int CustomerId { get; set; }
+    public DateTime SaleDate { get; set; }
+    public decimal TotalAmount { get; set; }
+    public string Status { get; set; } = "Pending";
+}
