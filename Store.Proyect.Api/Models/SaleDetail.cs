8ae56513a44f79dff92dc6e607d29277111fe126
maribel798-txt <lrs22110150@purisima.tecnm.mx>
2026-04-21 22:01:01 -0600

    de sales Details definir entities

---
+++ Store.Proyect.Api/Models/SaleDetail.cs
@@ -0,0 +1,11 @@
+﻿namespace Store.Proyect.Api.Models;
+
+public class SaleDetail
+{
+    public int Id { get; set; }
+    public int SaleId { get; set; }
+    public int ProductId { get; set; }
+    public int Quantity { get; set; }
+    public decimal UnitPrice { get; set; }
+    public decimal Subtotal { get; set; }
+}
