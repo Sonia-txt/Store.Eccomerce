ebbace6cb47255a7b84cb13c5e11d6ffb7baeb00
Sonia-txt <Soniaitzvaz@gmail.com>
2026-04-23 23:25:12 -0600

    Agregue contructores de los endpoint

--- Store.Proyect.Api/Program.cs
+++ Store.Proyect.Api/Program.cs
@@ -1,36 +1,52 @@
+using System.Data;
+using Store.Proyect.Api.DataAccess;
 using Store.Proyect.Api.DataAccess.Interfaces;
 using Store.Proyect.Api.Repositories;
+using Store.Proyect.Core.Interfaces;
 using Store.Proyect.Api.Repositories.Interfaces;
-using Store.Proyect.Api.DataAccess; 
+using Store.Proyect.Infrastructure.Repositories;
+using ProductRepository = Store.Proyect.Api.Repositories.ProductRepository;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
-builder.Services.AddOpenApi();
 builder.Services.AddControllers();
-
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 
-//builder.Services.AddSingleton<IProductCategoryRepository, InMemoryProductCategoryRepository>();
-builder.Services.AddScoped<IProductRepository, ProductRepository>();
-// Repositorios
-builder.Services.AddScoped<IProductRepository, ProductRepository>();
-builder.Services.AddScoped<ICustomerRepository, CustomerRepository>();
-builder.Services.AddScoped<IUserRepository, UserRepository>();         
-builder.Services.AddScoped<ISaleRepository, SaleRepository>();        
-builder.Services.AddScoped<ISaleDetailRepository, SaleDetailRepository>(); 
-
-
-builder.Services.AddScoped<IDbContext, Store.Proyect.Api.DataAccess.StoreDbContext>();
 builder.Services.AddScoped<IDbContext, StoreDbContext>();
 
+
+builder.Services.AddScoped<IDbConnection>(sp => 
+{
+    var context = sp.GetRequiredService<IDbContext>();
+    return context.Connection;
+});
+
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+
+builder.Services.AddScoped<ICustomerRepository, CustomerRepository>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<ISaleRepository, SaleRepository>();
+builder.Services.AddScoped<ISaleDetailRepository, SaleDetailRepository>();
+
 Dapper.DefaultTypeMap.MatchNamesWithUnderscores = true;
+
 var app = builder.Build();
 
-app.UseSwagger();
-app.UseSwaggerUI();
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Store API V1");
+    });
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
 
 app.MapControllers();
 
