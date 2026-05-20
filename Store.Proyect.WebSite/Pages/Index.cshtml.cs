ea1282dd577308e91c627c7e6312961e30c3d4d7
Sonia-txt <Soniaitzvaz@gmail.com>
2026-05-20 12:40:08 -0600

    files

---
+++ Store.Proyect.WebSite/Pages/Index.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Store.Project.WebSite.Pages;
+
+public class IndexModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
---
+++ Store.Proyect.WebSite/Pages/Privacy.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Store.Project.WebSite.Pages;
+
+public class PrivacyModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
